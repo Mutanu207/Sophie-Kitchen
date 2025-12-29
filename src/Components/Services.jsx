6d3e1065afca209d7bacbb81d6f1d8d3a2198a86
Mutanu207 <mutanumbuvi8@gmail.com>
2025-12-29 14:16:09 +0300

    add the map function to add information from data.jsx

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -4,22 +4,23 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import Data from '../Data/Data.jsx';
 
 export default function MediaCard() {
   return (
+    Data.map((service) => (
     <Card sx={{ maxWidth: 345 }}>
       <CardMedia
         sx={{ height: 140 }}
-        image="/static/images/cards/contemplative-reptile.jpg"
-        title="green iguana"
+        image={service.image}
+        title={service.title}
       />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
-          Lizard
+          {service.title}
         </Typography>
         <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
+          {service.description}
         </Typography>
       </CardContent>
       <CardActions>
@@ -27,5 +28,5 @@ export default function MediaCard() {
         <Button size="small">Learn More</Button>
       </CardActions>
     </Card>
-  );
+  )));
 }
