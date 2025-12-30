19ea79ee3a0173f0e490ffa94583ecd923c44b80
Mutanu207 <mutanumbuvi8@gmail.com>
2025-12-30 13:45:46 +0300

    Finished on the contact section wait to merge

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,6 +2,7 @@ import About from '../Components/About';
 import Footer from '../Components/Footer';
 import Hero from '../Components/Hero';
 import Box from '@mui/material/Box';
+import Contact from '../Components/Contact';
 function Home() {
   return (
     <Box>
